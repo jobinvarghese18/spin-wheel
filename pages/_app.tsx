08f988ace0cac286990a95dedc9c487c4ed616e0
Jobin T v <jobintv@focaloid.com>
2022-11-09 11:23:16 +0530

    feat: Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import React from 'react';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import '../styles/Home.module.css';
+import './index.css';
+import 'tailwindcss/tailwind.css';
+export default function App({
+  Component,
+  pageProps,
+}: AppProps): React.ReactNode {
+  return <Component {...pageProps} />;
 }
---
+++ src/components/5-pages/Home/Home.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const HomePage: React.FC = () => {
+  return <div className="w-full h-full bg-red-50">Home</div>;
+};
